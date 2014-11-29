0ecd4f7edddcd2faf7feaea1d45a219b50e797eb
Tom Mitchell <tom@tom.net.nz>
2014-11-28 16:14:37 -0800

    Add header

---
+++ kawaduino.h
@@ -0,0 +1,7 @@
+class Color {
+public:
+  Color() {};
+  Color(int r, int g, int b) : r(r), g(g), b(b) {};
+  int r, g, b;
+  uint32_t toUint32() { return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b; };
+};
